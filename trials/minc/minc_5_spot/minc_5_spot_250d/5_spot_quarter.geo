0f412f051141c6d740e377a8b89b8b7bdfc44067
Rohit Duggal <duggalrohi@myvuw.ac.nz>
2021-07-05 22:56:23 +1200

    T_vs_t script corrected_minc250d again error

--- trials/minc/minc_5_spot/minc_5_spot_250d/5_spot_quarter.geo
+++ trials/minc/minc_5_spot/minc_5_spot_250d/5_spot_quarter.geo
@@ -2,16 +2,16 @@
 x=500; //m
 y=500;
 z=-305; 
-lc=45; //element size or characteristic length for mesh
+lc=1; //element size or characteristic length for mesh
 MeshLayers = 1;
-n = 50; //required number of elements
+n = 20; //required number of elements
 inc = 1; //increment factor of mesh divisions
  
 // Geometry
-Point(1) = {0, 0, 0, lc*0.5};
-Point(2) = {x, 0, 0, lc*0.5};
-Point(3) = {x, y, 0, lc*0.5};
-Point(4) = {0, y, 0, lc*0.5};
+Point(1) = {0, 0, 0, lc};
+Point(2) = {x, 0, 0, lc};
+Point(3) = {x, y, 0, lc};
+Point(4) = {0, y, 0, lc};
 Line(1) = {1, 2};				// bottom line
 Line(2) = {2, 3};				// right line
 Line(3) = {3, 4};				// top line
@@ -20,10 +20,8 @@ Line Loop(1) = {1, 2, 3, 4};
 Plane Surface(1) = {1};
 
 //In case of progression:
-Transfinite Curve {1} = n+1 Using Progression inc;
-Transfinite Curve {2} = n+1 Using Progression inc;
-Transfinite Curve {3} = n+1 Using Progression inc;
-Transfinite Curve {4} = n+1 Using Progression inc;
+Transfinite Curve {1,2,3,4} = n+1 Using Progression inc;
+
 
 //In case of bump (refine near corners):
 // Transfinite Curve {1} = n+1 Using Bump inc/20;
--- trials/minc/minc_5_spot/minc_5_spot_250d/T_vs_t_plot.py
+++ trials/minc/minc_5_spot/minc_5_spot_250d/T_vs_t_plot.py
@@ -6,6 +6,7 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import math
 
 '''File name'''
 out = h5py.File(sys.argv[1])
@@ -14,28 +15,42 @@ out = h5py.File(sys.argv[1])
 n=np.int64(sys.argv[4]) 
 m=np.int64(sys.argv[5]) #value at every diagonal element
 
-Tv=[268.77, 283.28, 290.75, 294.38, 290.74, 281.84, 261.59, 221.49, 167.41, 127.89, 116.71, 116.06]
-Xv=[18.1, 47.63, 101.99, 176.47, 248.75, 318.88, 385.69, 458.49, 527.84, 606.46, 656.23, 686.14]
 
-X=np.linspace(671.65,35.35,20)
+'''theory data from 1982-bodvarsson'''
+xLit250d=[18.19, 46.76, 103.24, 174.1, 247.77, 317.48, 384.4, 459.02, 526.97, 604.77, 658.66, 687.55]
+PLit250d=[6.14, 6.48, 6.75, 6.93, 7.05, 7.11, 7.25, 7.36, 7.53, 7.83, 8.29, 8.84]
+xTLit250d=[19.31, 48.08, 103.96, 175.17, 250.1, 319.98, 384.68, 460.93, 527.44, 605.72, 656.42, 689.1]
+TLit250d=[274.89, 269.87, 266.84, 260.57, 250.79, 235.19, 210.57, 184.75, 156.92, 133.72, 121.67, 117.81]
 
-y = out['cell_fields/fluid_'+sys.argv[2]][-1,0:n]
-Y=y[0::m]
 
-y1 = out['cell_fields/fluid_'+sys.argv[3]][-1,0:n]
-Y1=y1[0::m]
+
+'''waiwera results'''
+'''for pressure'''
+P1 = out['cell_fields/fluid_'+sys.argv[2]][-1,0:n] 
+P=P1[0::m]
+'''for temperature''' 
+T1=out['cell_fields/fluid_'+sys.argv[3]][-1,0:n] 
+T=T1[0::m]
+
+'''method to create x-axis for the plot'''
+xdim=500 #model dimension e.g., here is 500 by 500
+div=2*(m-1) #just a variable to get the location of first cell center
+b=np.sqrt(2) * (xdim/div) #diagonal distance of first cell center
+c=(np.sqrt(2) * xdim) - b #diagonal distance between cell centers of last and first cells
+X=np.linspace(c,b,len(P)) #divisions created to plot Pressure or Temperature 
+
 
 fig, ax1=plt.subplots()
-
-ax1.plot(X,Y1,'',label='T')
+ax1.plot(X,P*1e-6,'',label='P'+sys.argv[2])
 
 ax2=ax1.twinx()
+# ax3=ax1.twinx()
+ax2.plot(xLit250d,PLit250d,'',label='theory')
+# ax3.plot(X,T,'',label=sys.argv[3])
 
-ax2.plot(X,Y,'',label='y')
 
-# ax2.plot(Xv,Tv,'',label='T_v')
 fig.tight_layout()
 plt.legend()
 plt.show()
 for i in range(0,20):
-    print(X[i], Y[i], Y1[i])
+    print(X[i], P[i], T[i])
